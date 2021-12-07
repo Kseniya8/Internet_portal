61f2dcc0162375f921f78da9a1fc102613d711fa
Kseniya8 <badretdinova.xenia@yandex.ru>
2021-12-07 22:55:33 +0300

    Created search for partners

--- controllers/partnerController.js
+++ controllers/partnerController.js
@@ -10,13 +10,8 @@ exports.index = (req, res) => res.render("partners.html", {
 exports.searchForms = (req, res, next) => {
 
     name_fields = [
-        'name',
-        'surname',
-        'patronymic',
-        'education.group',
-        'education.year_graduation',
-        'education.specialty',
-        'education.focus',
+        'partner.name',
+        'partner.year',
         'city',
     ]
     params = { status_verified: true, isPartner: true }
@@ -31,20 +26,13 @@ exports.searchForms = (req, res, next) => {
     need_fields = {
         '_id': 1,
         'photo': 1,
-        'name': 1,
-        'surname': 1,
-        'patronymic': 1,
+        'partner.name': 1,
         'city': 1,
-        'education.year_graduation': 1,
+        'partner.year': 1,
     }
 
     formModelPartner.find(params, need_fields, { skip: skip, limit: 10 })
         .then(forms => {
-            for (let i = 0; i < forms.length; i++) {
-                if (forms[i]['education'].length > 0)
-                    forms[i]['education'] = forms[i]['education'][forms[i]['education'].length - 1]
-
-            }
             if (!skip) {
                 formModelPartner.find(params).countDocuments()
                     .then(count => res.json({ forms: forms, count_pages: Math.ceil(count / 10) }))
@@ -54,27 +42,3 @@ exports.searchForms = (req, res, next) => {
         .catch(err => next(err))
 }
 
-exports.getForm = (req, res, next) => {
-
-    let need_fields = {}
-    let full = true
-
-    if (req.session.role === 'guest' || !req.session.role) {
-        need_fields = { name: 1, surname: 1, patronymic: 1, photo: 1 }
-        full = false
-    }
-
-    formModel.findById(req.params['id'], need_fields)
-        .then(form => {
-            form = form._doc
-            form.full = full
-
-            res.render("account.html", {
-                [req.session.lang ?? 'ru']: true,
-                form: form, isAuth: Boolean(req.session.user_id),
-                isAdmin: Boolean(req.session.role == "admin")
-            })
-        })
-        .catch(err => next(err))
-
-}
--- public/javascripts/partners.js
+++ public/javascripts/partners.js
@@ -3,24 +3,23 @@ import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.js
 let vueInput;
 window.onload = function () {
 
-    var yearsArray = [];
-    for (var i = new Date().getFullYear(); i >= 1971; i--) {
-        yearsArray.push(i);
-    }
     vueInput = new Vue({
         el: '#mainBlock',
         data: {
             partner_name: '', partner_year: '',
             lastPage: 0,
             correntPage: 0,
-            yearsArray,
             inputData: []
+        },
+        computed: {
+            visibleNoResult: function () {
+                return this.inputData.length == 0;
+            }
         },
         methods: {
             searchButtonClick: function () {
-                this.address = '/forms/search?name=' + this.name +
-                    '&inputData.partner.name=' + this.partner_name +
-                    '&inputData.partner.year=' + this.partner_year;
+                this.address = '/partners/search?partner.name=' + this.partner_name +
+                    '&partner.year=' + this.partner_year;
                 this.MoveOnPage(1);
             },
             MoveOnPage: function (page) {
@@ -41,10 +40,10 @@ window.onload = function () {
                 this.inputData = [];
                 answer.forEach(item => {
                     this.inputData.push({
-                        full_name: item.surname + ' ' + item.name + ' ' + item.patronymic,
+                        partner_name: item.partner['name'],
                         id: item._id,
                         photo: item.photo,
-                        year_graduation: !item.education[0] ? '' : item.education[0].year_graduation,
+                        year: item.partner.year,
                         city: item.city
                     });
                 });
