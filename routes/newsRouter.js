2700b02d2ea144a086551f95d4482bb7b34fe2db
Alina_Kuznetsova <eyedalina@gmail.com>
2022-06-11 12:21:34 +0300

    Update newsRouter.js

--- routes/newsRouter.js
+++ routes/newsRouter.js
@@ -5,6 +5,10 @@ const newsRouter = express.Router();
 const jsonParser = express.json();
 
 newsRouter.get('/', newsController.index);
-newsRouter.get('/admin', jsonParser, newsController.admin)
+
+newsRouter.get('/add_news', jsonParser, newsController.AddNews)
+newsRouter.get('/edit_news', jsonParser, newsController.EditNews)
+newsRouter.get('/edit_section', jsonParser, newsController.EditSection)
+newsRouter.delete('/delete_news', jsonParser, newsController.deleteNews)
 
 module.exports = newsRouter;
